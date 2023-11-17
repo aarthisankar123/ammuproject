f10999c5f8aff9dd0294ebbf444c963e838fa377
aarthisankar123 <aarthiece200@gmail.com>
2023-11-17 10:14:36 +0530

    my luck

---
+++ src/test/java/org/test/Sad.java
@@ -0,0 +1,16 @@
+package org.test;
+
+import org.testng.annotations.Test;
+
+public class Sad {
+	@Test
+	private void test() {
+		System.out.println("test 67");
+	}
+	
+	@Test
+	private void test1() {
+		System.out.println("test 87");
+	}
+
+}
